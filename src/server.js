5dcfb5930d5c194bb6c494a1b05e23690e7dda30
Sacha Sedriks <sachased@gmail.com>
2020-11-14 15:12:21 -0800

    Update migration.sql comments table and server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const knex = require('knex')
 const app = require('./app')
-const { PORT,  } = require('./config')
+const { PORT, DATABASE_URL } = require('./config')
 
 const db = knex({
   client: 'pg',
