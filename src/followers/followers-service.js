71dc040cf5cc6dade7bf93c63ffcb3dc193784cd
Michaela Bateman <michaela.bateman001@gmail.com>
2020-11-17 18:03:43 -0600

    created a followers route to get who is following you and a following route to get who you are following

--- src/followers/followers-service.js
+++ src/followers/followers-service.js
@@ -2,20 +2,27 @@ const followersService = {
 
     // get all
     getFollowers(db, follower_id) {
+        console.log(follower_id)
         return db
             .from('followers')
             .select('*')
-            .where('followers.follower_id', follower_id)
+            .where('followers.follower_user_id', follower_id)
     },
     // get by id
-    getFollowerById(db, followerId) {
+    getFollowingById(db, userid) {
         return db
             .from('followers')
             .select('follower_user_id', 'username')
             .join('users', {'users.id': 'followers.follower_user_id'})
-            .where('followers.followed_user_id', followerId)
+            .where('followers.followed_user_id', userid)
     },
-
+    getFollowersById(db, userid){
+        return db
+            .from('followers')
+            .select('followed_user_id', 'username')
+            .join('users', {'users.id': 'followers.followed_user_id'})
+            .where('followers.follower_user_id', userid)
+    },
     // post
     followUser(db, followed_user_id, follower_user_id) {
         return db.insert({
@@ -31,7 +38,7 @@ const followersService = {
         return db('followers')
             .where({ followed_user_id, follower_user_id })
             .delete()
-    }
+    },
 }
 
 module.exports = followersService;
