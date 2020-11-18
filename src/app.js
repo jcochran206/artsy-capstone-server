66f615b8b89ca22baf5b7ce80344ccdb2c2bef15
Sacha Sedriks <sachased@gmail.com>
2020-11-17 21:00:07 -0800

    Tried to fix

--- src/app.js
+++ src/app.js
@@ -36,9 +36,9 @@ app.use(function errorHandler(error, req, res, next) {
     if (NODE_ENV === 'production') {
         response = { error: { message: 'server error' } }
     } else {
-        console.error(error)
         response = { message: error.message, error }
     }
+    console.error(error)
     res.status(500).json(response)
 })
 
