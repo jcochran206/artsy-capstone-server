845ad7ceee33c60316c13f03a0f87242e33d8c6a
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2020-11-17 19:37:29 -0800

    modified post-router and removed error

--- src/posts/posts-router.js
+++ src/posts/posts-router.js
@@ -105,21 +105,7 @@ postRouter
             })
             .catch(next)
     })
-    //joined get 
-    // .get((req, res, next) => {
-    //     const { id } = req.params;
-    //     postService.getPostsByUserId(req.app.get('db'), id)
-    //         .then(post => {
-    //             if (!post) {
-    //                 return res
-    //                     .status(404)
-    //                     .send({ error: { message: 'user and post id does not match' } })
-    //             }
-    //             res.json(serializePost(post))
-    //             next()
-    //         })
-    //         .catch(next)
-    // })
+
     //update
     .put(jsonParser, (req, res, next) => {
         const { title, desc_post } = req.body;
