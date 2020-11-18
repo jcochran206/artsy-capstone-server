e36fe569bd3c14878916776bc77be32b58094a79
Jerrad Moon <jerradm85@gmail.com>
2020-11-17 19:35:43 -0800

    update for CLIENT ORIGIN for heroku

--- src/posts/posts-router.js
+++ src/posts/posts-router.js
@@ -54,14 +54,6 @@ postRouter
     })
 
 postRouter
-    .route('/feed')
-    .get((req, res, next) => {
-        const user_id = req.user.id
-
-        postService.getPosts
-    })
-
-postRouter
     .route('/feed/:id')
     .get((req, res, next) => {
         const { id } = req.params;
@@ -105,21 +97,7 @@ postRouter
             })
             .catch(next)
     })
-    //joined get 
-    // .get((req, res, next) => {
-    //     const { id } = req.params;
-    //     postService.getPostsByUserId(req.app.get('db'), id)
-    //         .then(post => {
-    //             if (!post) {
-    //                 return res
-    //                     .status(404)
-    //                     .send({ error: { message: 'user and post id does not match' } })
-    //             }
-    //             res.json(serializePost(post))
-    //             next()
-    //         })
-    //         .catch(next)
-    // })
+    
     //update
     .put(jsonParser, (req, res, next) => {
         const { title, desc_post } = req.body;
